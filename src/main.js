90fa275ba843a06bae1445d3ffa9b4fb737517af
ikemura <banbara.work@gmail.com>
2017-06-29 15:31:22 +0900

    Add onsenui vue-onsenui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// Webpack CSS import
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+
+// JS import
+import 'onsenui'
+import VueOnsen from 'vue-onsenui'
+
+Vue.use(VueOnsen)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
